5fad50f80770e501c9b27aacc8191f28a387b93b
Amphiluke <amphiluke@gmail.com>
2016-01-09 22:54:28 +0600

    Create floatingScroll live demos webpage

---
+++ js/main.js
@@ -0,0 +1,42 @@
+(function (global) {
+
+"use strict";
+
+var $ = global.jQuery;
+
+$(".fs-demo").floatingScroll();
+
+
+$(".fs-whatwg-cols").on("change", "input[type='checkbox']", function (e) {
+    var $checkboxes = $(e.delegateTarget).find("input[type='checkbox']"),
+        index;
+    if ($checkboxes.filter(":checked").length) {
+        index = $checkboxes.index(e.target);
+        if (index > -1) {
+            $("#fs-whatwg").find("th, td")
+                .filter(":nth-child(" + (index + 2) + ")")
+                .toggleClass("hidden");
+        }
+    } else {
+        $checkboxes.prop("checked", true);
+        $("#fs-whatwg").find("th.hidden, td.hidden").removeClass("hidden");
+    }
+    $(".fs-whatwg").floatingScroll("update");
+});
+
+
+$("#fs-listing-toggle").on("click", function () {
+    var $scrollOwner = $(".fs-listing");
+    if ($scrollOwner.hasClass("fs-listing-collapsed")) {
+        $scrollOwner.removeClass("fs-listing-collapsed").floatingScroll("init");
+    } else {
+        $scrollOwner.addClass("fs-listing-collapsed").floatingScroll("destroy");
+    }
+});
+
+
+if (global.hljs) {
+    global.hljs.initHighlightingOnLoad();
+}
+
+})(this);
