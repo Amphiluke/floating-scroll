726b4b987ab35e227430b4603df2033e2b26cf62
Amphiluke <amphiluke@gmail.com>
2017-01-02 23:30:16 +0600

    Local floating scroll bar for positioned blocks

--- src/jquery.floatingscroll.js
+++ src/jquery.floatingscroll.js
@@ -1,11 +1,11 @@
 /*!
- * jQuery floatingScroll Plugin v2.2.5
+ * jQuery floatingScroll Plugin v2.3.0
  * supported by jQuery v1.4.3+
  *
  * https://github.com/Amphiluke/floating-scroll
  * http://amphiluke.github.io/floating-scroll/
  *
- * Copyright (c) 2011-2016 Amphiluke
+ * Copyright (c) 2011-2017 Amphiluke
  */
 (function (global, factory) {
     "use strict";
@@ -17,37 +17,40 @@
         factory(global.jQuery);
     }
 }(this, function ($) {
-
     "use strict";
-    
+
     var wnd = window,
-        getMaxVisibleY = /*@cc_on (@_jscript_version<9)?function(){var e=document.documentElement;return e.scrollTop+e.clientHeight;}:@*/function () {return wnd.pageYOffset + wnd.innerHeight;};
-    
+        getMaxVisibleY = /*@cc_on (@_jscript_version<9)?function(){var e=document.documentElement;return e.scrollTop+e.clientHeight;}:@*/function () {return wnd.pageYOffset + wnd.innerHeight;},
+        getMaxVisibleElY = function ($el) {return $el.offset().top + $el.scrollTop() + $el.height();};
+
     function FScroll(cont) {
-        var inst = this;
+        var inst = this,
+            scrollBody = cont.closest(".fl-scrolls-body");
         inst.cont = {block: cont[0], left: 0, top: 0, bottom: 0, height: 0, width: 0};
+        if (scrollBody.length) {
+            inst.scrollBody = scrollBody;
+        }
         inst.sbar = inst.initScroll();
         inst.visible = true;
         inst.updateAPI(); // recalculate floating scrolls and hide those of them whose containers are out of sight
         inst.syncSbar(cont[0]);
         inst.addEventHandlers();
     }
-    
+
     $.extend(FScroll.prototype, {
-    
         initScroll: function () {
             var flscroll = $("<div class='fl-scrolls'></div>");
             $("<div></div>").appendTo(flscroll).css({width: this.cont.block.scrollWidth + "px"});
             return flscroll.appendTo(this.cont.block);
         },
-    
+
         addEventHandlers: function () {
             var inst = this,
                 handlers,
                 i, len;
             handlers = inst.eventHandlers = [
                 {
-                    $el: $(wnd),
+                    $el: inst.scrollBody || $(wnd),
                     handlers: {
                         // Don't use `$.proxy()` since it makes impossible event unbinding individually per instance
                         // (see the warning at http://api.jquery.com/unbind/)
@@ -89,23 +92,23 @@
                 handlers[i].$el.bind(handlers[i].handlers);
             }
         },
-    
+
         checkVisibility: function () {
             var inst = this,
                 cont = inst.cont,
                 mustHide = (inst.sbar[0].scrollWidth <= inst.sbar[0].offsetWidth),
                 maxVisibleY;
             if (!mustHide) {
-                maxVisibleY = getMaxVisibleY();
+                maxVisibleY = inst.scrollBody ? getMaxVisibleElY(inst.scrollBody) : getMaxVisibleY();
                 mustHide = ((cont.bottom <= maxVisibleY) || (cont.top > maxVisibleY));
             }
             if (inst.visible === mustHide) {
-                inst.visible = !inst.visible;
+                inst.visible = !mustHide;
                 // we cannot simply hide a floating scroll bar since its scrollLeft property will not update in that case
                 inst.sbar.toggleClass("fl-scrolls-hidden");
             }
         },
-    
+
         syncCont: function (sender, preventSyncSbar) {
             // Prevents next syncSbar function from changing scroll position
             if (this.preventSyncCont === true) {
@@ -115,7 +118,7 @@
             this.preventSyncSbar = !!preventSyncSbar;
             this.cont.block.scrollLeft = sender.scrollLeft;
         },
-    
+
         syncSbar: function (sender, preventSyncCont) {
             // Prevents next syncCont function from changing scroll position
             if (this.preventSyncSbar === true) {
@@ -125,7 +128,7 @@
             this.preventSyncCont = !!preventSyncCont;
             this.sbar[0].scrollLeft = sender.scrollLeft;
         },
-    
+
         // Recalculate scroll width and container boundaries
         updateAPI: function () {
             var inst = this,
@@ -137,11 +140,14 @@
             cont.left = pos.left;
             cont.top = pos.top;
             cont.bottom = pos.top + cont.height;
-            inst.sbar.width(cont.width).css("left", pos.left + "px");
+            inst.sbar.width(cont.width);
+            if (!inst.scrollBody) {
+                inst.sbar.css("left", pos.left + "px");
+            }
             $("div", inst.sbar).width(block[0].scrollWidth);
             inst.checkVisibility(); // fixes issue #2
         },
-    
+
         // Remove a scrollbar and all related event handlers
         destroyAPI: function () {
             var handlers = this.eventHandlers,
@@ -151,9 +157,8 @@
             }
             this.sbar.remove();
         }
-    
     });
-    
+
     // `attachScroll` is the old alias used in v1.X. Temporally kept for backward compatibility.
     $.fn.attachScroll = $.fn.floatingScroll = function (method) {
         if (!arguments.length || method === "init") {
@@ -165,5 +170,4 @@
         }
         return this;
     };
-
 }));
